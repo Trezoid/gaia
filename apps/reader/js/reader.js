728178c7b5eec32c8cb2f511cf4ea29d59076166
Liam Davis <liam.davis@internode.on.net>
2012-02-29 21:26:12 +1100

    fixed ePub handling, applying consistent style

--- apps/reader/js/reader.js
+++ apps/reader/js/reader.js
@@ -13,56 +13,40 @@ function update(file)
   };
   reader.readAsBinaryString(file.item(0));
 }
+
 var book;
 function createBook(epub)
 {
-      book = new JSEpub(epub);
+  book = new JSEpub(epub);
+  book.processInSteps(function(step, extras)
+  {
+    if(step === 5) { 
+      var style = document.createElement("style");
+      style.id="pageStyle";
+      document.body.appendChild(style);
 
-book.processInSteps(function(step, extras)
-{
-  var msg;
-  if (step === 1) {
-    msg = "unzipping ";
-  } else if (step === 2) {
-    msg = "uncompressing " + extras;
-  } else if (step === 3) {
-    msg = "Reading OPF";
-  } else if (step === 4) {
-    msg = "Post processing";
-  } else if (step === 5) {
-    msg = "Finishing";
-   showPage(0);
-  } else {
-    msg = "An error occured: " + step + extras;
+      var fileBox = document.getElementById("fileHolder");
+      fileBox.style.display = 'none';
+      showPage();
     }
-  console.log(msg)
-}); 
-  
-
+  }); 
 }
 
-function showPage(page) {
+var page = 0;
+function showPage() {
+  document.getElementById('pageStyle').innerHTML = "";
   var spine = book.opf.spine[page];
   var href = book.opf.manifest[spine]['href'];
   var doc = book.files[href];
   var html = new XMLSerializer().serializeToString(doc);
   var bookBox = document.getElementById("pages");
   bookBox.innerHTML = html;
-  bookBox.addEventListener('click', function() {showPage(page + 1);}, false);
   var ratio = window.innerHeight / window.innerWidth;
-  var totalHeight = (window.innerHeight + window.scrollMaxY);
-  var style = '#pages {-moz-column-width: '+(window.innerWidth - 50)+'px!important; width: '+totalHeight / ratio+'px!important;-moz-column-gap:20px;}';
-  if(document.getElementById("pageStyle") === null)
-  {
-    var newStyle = document.createElement("style");
-    newStyle.id = "pageStyle"
-    newStyle.innerHTML = style;
-    document.body.appendChild(newStyle);
-  } else {
+  var totalHeight = window.innerHeight + window.scrollMaxY;
+  var style = '#pages {-moz-column-width: '+(window.innerWidth - (window.innerWidth * 0.2))+'px!important; width: '+((totalHeight / ratio) + window.innerHeight / ratio)+'px!important;-moz-column-gap: 100px;padding-right: 100px;}';
     document.getElementById("pageStyle").innerHTML = style;
-  }
-  var fileBox = document.getElementById("fileHolder");
-  fileBox.style.display = 'none';
+  page += 1;
+  document.getElementById('pages').addEventListener('click', showPage, false);
 }
 
 
