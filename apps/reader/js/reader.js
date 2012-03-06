dbada7cd2e0f44edf123cb3970cf02139ca6f7c1
Liam Davis <liam.davis@internode.on.net>
2012-03-06 17:22:48 +1100

    Better pages, better flipping through pages, better file selector screen

--- apps/reader/js/reader.js
+++ apps/reader/js/reader.js
@@ -7,11 +7,12 @@
 function update(file)
 {
   var reader = new FileReader();
+  reader.readAsBinaryString(file.item(0));
+
   reader.onload = function(event)
   {
     createBook(reader.result);
   };
-  reader.readAsBinaryString(file.item(0));
 }
 
 var book;
@@ -20,39 +21,83 @@ function createBook(epub)
   book = new JSEpub(epub);
   book.processInSteps(function(step, extras)
   {
-    if(step === 5) { 
-      var style = document.createElement("style");
-      style.id="pageStyle";
+    if (step === 5) {
+      var style = document.createElement('style');
+      style.id = 'pageStyle';
       document.body.appendChild(style);
 
-      var fileBox = document.getElementById("fileHolder");
+      var fileBox = document.getElementById('fileHolder');
       fileBox.style.display = 'none';
-      showPage();
+      showChapter();
     }
-  }); 
+  });
 }
 
 var page = 0;
-function showPage() {
-  document.getElementById('pageStyle').innerHTML = "";
-  var spine = book.opf.spine[page];
+var chapter = 0;
+function showChapter() {
+  document.getElementById('pageStyle').innerHTML = '';
+  if (!book.opf.spine[chapter])
+  {
+    book = null;
+    document.getElementsByTagName('input')[0].value = '';
+    document.getElementById('fileHolder').style.display = 'block';
+    document.getElementById('pages').innerHTML = '';
+    chapter = 0;
+    page = 0;
+    return;
+  }
+  var spine = book.opf.spine[chapter];
   var href = book.opf.manifest[spine]['href'];
   var doc = book.files[href];
   var html = new XMLSerializer().serializeToString(doc);
-  var bookBox = document.getElementById("pages");
+  var bookBox = document.getElementById('pages');
   bookBox.innerHTML = html;
-  var ratio = window.innerHeight / window.innerWidth;
-  var totalHeight = window.innerHeight + window.scrollMaxY;
-  var style = '#pages {-moz-column-width: '+(window.innerWidth - (window.innerWidth * 0.2))+'px!important; width: '+((totalHeight / ratio) + window.innerHeight / ratio)+'px!important;-moz-column-gap: 100px;padding-right: 100px;}';
-    document.getElementById("pageStyle").innerHTML = style;
+
+  var totalHeight = (window.innerHeight + window.scrollMaxY);
+  var contentWidth = window.innerWidth;
+
+  if (totalHeight - 40 > window.innerHeight)
+  {
+    contentWidth = (window.innerWidth > window.innerHeight) ?
+      ((totalHeight / window.innerHeight) * window.innerWidth) + (window.innerWidth) :
+      ((totalHeight / window.innerHeight) * window.innerHeight) + window.innerHeight;
+    var numCols = Math.ceil(contentWidth / (window.innerWidth - 80));
+    contentWidth = numCols * window.innerWidth;
+  }
+  var style = '#pages{' +
+    ' -moz-column-width: ' + (window.innerWidth - 80) + 'px!important;' +
+    ' width: ' + contentWidth + 'px!important;' +
+    '-moz-column-gap: 80px;' +
+    'margin-left: 40px;}';
+
+    document.getElementById('pageStyle').innerHTML = style;
+  chapter += 1;
+  document.addEventListener('click', nextPage, false);
+}
+
+function nextPage() {
+  var pages = document.getElementById('pages');
+  if (((page + 1) * (window.innerWidth + 2)) >= (window.innerWidth + window.scrollMaxX))
+  {
+    showChapter();
+    page = 0;
+    pages.setAttribute('style', '-moz-transform: translate(0, 0);');
+    return;
+  }
+  //var direction = forward ? 1 : -1;
+  pages.setAttribute('style', '-moz-transform: translate(-' +
+        ((page + 1) * (window.innerWidth + 1)) + 'px, 0);');
+
   page += 1;
-  document.getElementById('pages').addEventListener('click', showPage, false);
 }
 
 
 window.onload = function()
 {
   var fileBox = document.getElementsByTagName('input')[0];
-  fileBox.addEventListener('change', function() {update(fileBox.files);}, false);
-}
+  fileBox.addEventListener('change',
+      function() {update(fileBox.files);},
+      false);
+};
 
