e6b8fd2c8c0c3e2a0984e87de2b1acd0f971b055
Liam Davis <liam.davis@internode.on.net>
2012-03-08 11:14:41 +1100

    Page swiping

--- apps/reader/js/reader.js
+++ apps/reader/js/reader.js
@@ -3,16 +3,21 @@
 /*
  * requires js-epub, found at https://github.com/augustl/js-epub
  */
-
+var mover = null;
 function update(file)
 {
   var reader = new FileReader();
   reader.readAsBinaryString(file.item(0));
-
+  
   reader.onload = function(event)
   {
     createBook(reader.result);
   };
+
+  document.addEventListener('mousedown', function(evt) {mover = new moving(evt);}, false);
+    document.addEventListener('mousemove', function(evt) {mover.mouseMove(evt);}, false);
+    document.addEventListener('mouseup', function(evt) {mover.mouseEnd(evt);}, false);
+  
 }
 
 var book;
@@ -73,25 +78,60 @@ function showChapter() {
 
     document.getElementById('pageStyle').innerHTML = style;
   chapter += 1;
-  document.addEventListener('click', nextPage, false);
 }
 
-function nextPage() {
+function nextPage(dir) {
   var pages = document.getElementById('pages');
-  if (((page + 1) * (window.innerWidth + 2)) >= (window.innerWidth + window.scrollMaxX))
+  if (((page + dir) * (window.innerWidth + 2)) >= (window.innerWidth + window.scrollMaxX))
   {
     showChapter();
     page = 0;
     pages.setAttribute('style', '-moz-transform: translate(0, 0);');
     return;
   }
-  //var direction = forward ? 1 : -1;
   pages.setAttribute('style', '-moz-transform: translate(-' +
-        ((page + 1) * (window.innerWidth + 1)) + 'px, 0);');
+        ((page + dir) * (window.innerWidth + 1)) + 'px, 0);');
 
   page += 1;
 }
 
+function showMenu() {
+  /*
+   * TODO: Make menus for useful functions
+   * Primarily back to menu, page jump, bookmark(?)
+   */
+}
+
+var moving = function(evt) {
+  this.startTime = evt.timeStamp;
+  this.startX = evt.screenX;
+  this.active = true;
+}
+
+moving.prototype = {
+  mouseMove: function(callingEvt) {
+    if(this.active === true)
+    {
+        var offset = callingEvt.screenX - this.startX;
+       document.getElementById('pages').style.MozTransform= 'translateX(-'+((page * (window.innerWidth + 1)) + (-1 * offset))+'px)';
+    }
+  },
+
+  mouseEnd: function (callingEvt) {
+      this.active = false;
+      var offset = callingEvt.screenX - this.startX;
+      if((callingEvt.timeStamp - this.startTime) < 30) {
+        showMenu();
+    } else if((-1 * offset > (window.innerWidth / 8))) {
+        var dir = offset > 0 ? -1 : 1;
+        nextPage(dir);
+        mover = null;
+    } else  {
+        document.getElementById('pages').style.MozTransform = 'translateX(-'+(page * (window.innerWidth + 1)) + 'px)';
+    }
+      
+  }
+}
 
 window.onload = function()
 {
